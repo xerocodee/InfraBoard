17308c03df9a6a82d61b739f93b75917a64995c4
Pratik Pimpalkar <pratik@xerocodee.com>
2024-01-24 12:28:21 +0530

    AWS Data added

--- app/store/leftSideBarData.ts
+++ app/store/leftSideBarData.ts
@@ -1,119 +1,312 @@
 export const leftSideBarData = {
-  aws: {
-    tabs: [
-      {
-        title: 'Compute',
-        subTabs: [
-          {
-            title: 'App Runner',
-          },
-          {
-            title: 'AutoScaling',
-          },
-          {
-            title: 'Batch',
-          },
-          {
-            title: 'Containers',
-          },
-          {
-            title: 'EC2',
-          },
-          {
-            title: 'Elastic Beanstalk',
-          },
-          {
-            title: 'Image Builder',
-          },
-          {
-            title: 'Lamda',
-          },
-          {
-            title: 'Lightsail',
-          },
+    aws: {
+        tabs: [
+            {
+                title: 'Compute',
+                subTabs: [
+                    {
+                        title: 'App Runner',
+                    },
+                    {
+                        title: 'AutoScaling',
+                    },
+                    {
+                        title: 'Batch',
+                    },
+                    {
+                        title: 'Containers',
+                    },
+                    {
+                        title: 'EC2',
+                    },
+                    {
+                        title: 'Elastic Beanstalk',
+                    },
+                    {
+                        title: 'Image Builder',
+                    },
+                    {
+                        title: 'Lamda',
+                    },
+                    {
+                        title: 'Lightsail',
+                    },
+                ],
+            },
+            {
+                title: 'Network',
+                subTabs: [
+                    {
+                        title: 'API Gateway',
+                    },
+                    {
+                        title: 'AppMesh',
+                    },
+                    {
+                        title: 'CloudFront',
+                    },
+                    {
+                        title: 'Direct Connect',
+                    },
+                    {
+                        title: 'Global Accelerator',
+                    },
+                    {
+                        title: 'Load Balancers',
+                    },
+                    {
+                        title: 'Network Firewall',
+                    },
+                    {
+                        title: 'Network Manager',
+                    },
+                    {
+                        title: 'Route 53',
+                    },
+                    {
+                        title: 'Transit Gateway',
+                    },
+                    {
+                        title: 'VPC',
+                    },
+                    {
+                        title: 'VPN',
+                    },
+                ],
+            },
+            {
+                title: 'Storage',
+                subTabs: [
+                    {
+                        title: 'Archieve & Backup',
+                    },
+                    {
+                        title: 'Backup',
+                    },
+                    {
+                        title: 'File System',
+                    },
+                    {
+                        title: 'S3',
+                    },
+                    {
+                        title: 'Storage Gateway',
+                    },
+                ],
+            },
+            {
+                title: 'Database',
+                subTabs: [
+                    {
+                        title: 'DMS',
+                    },
+                    {
+                        title: 'DocumentDB',
+                    },
+                    {
+                        title: 'Dynamo',
+                    },
+                    {
+                        title: 'ElastiCache',
+                    },
+                    {
+                        title: 'Keyspaces Keyspace',
+                    },
+                    {
+                        title: 'MemoryDB',
+                    },
+                    {
+                        title: 'Neptune',
+                    },
+                    {
+                        title: 'QLDB',
+                    },
+                    {
+                        title: 'RDS',
+                    },
+                    {
+                        title: 'SimpleDB Domain',
+                    },
+                    {
+                        title: 'Timestream Write',
+                    },
+                ],
+            },
+            {
+                title: 'Security & Identity',
+                subTabs: [
+                    {
+                        title: 'Access analyzer',
+                    },
+                    {
+                        title: 'Account',
+                    },
+                    {
+                        title: 'ACM PCA',
+                    },
+                    {
+                        title: 'Audit Manager',
+                    },
+                    {
+                        title: 'Cognito',
+                    },
+                    {
+                        title: 'Detective',
+                    },
+                    {
+                        title: 'Director    Service',
+                    },
+                    {
+                        title: 'Firewall Manager (FMS)',
+                    },
+                    {
+                        title: 'GuardDuty',
+                    },
+                    {
+                        title: 'IAM',
+                    },
+                    {
+                        title: 'KMS',
+                    },
+                    {
+                        title: 'Secrets Manager',
+                    },
+                    {
+                        title: 'Security Hub',
+                    },
+                    {
+                        title: 'Shield',
+                    },
+                    {
+                        title: 'Signer',
+                    },
+                    {
+                        title: 'SSO Admin',
+                    },
+                    {
+                        title: 'SSO Identity Store',
+                    },
+                    {
+                        title: 'WAF',
+                    },
+                ],
+            },
+            {
+                title: 'Tools',
+                subTabs: [
+                    { title: "Budgets" },
+                    { title: "CE (Cost Explorer)" },
+                    { title: "Clean Rooms" },
+                    { title: "Cloud9" },
+                    { title: "Cloud Control" },
+                    { title: "CloudFormation" },
+                    { title: "CloudTrail" },
+                    { title: "CloudWatch" },
+                    { title: "CodeArtifact" },
+                    { title: "CodeBuild" },
+                    { title: "CodeCatalyst" },
+                    { title: "CodeCommit" },
+                    { title: "CodeDeploy" },
+                    { title: "CodeGuru" },
+                    { title: "CodePipeline" },
+                    { title: "CodeStar" },
+                    { title: "Config" },
+                    { title: "Control Tower" },
+                    { title: "DataSync" },
+                    { title: "FinSpace" },
+                    { title: "Grafana" },
+                    { title: "License Manager" },
+                    { title: "MQ" },
+                    { title: "OpsWorks" },
+                    { title: "Organizations" },
+                    { title: "Pinpoint" },
+                    { title: "Prometheus (AMP)" },
+                    { title: "Resource Explorer" },
+                    { title: "Resource Groups" },
+                    { title: "Service Catalog" },
+                    { title: "Service Discovery" },
+                    { title: "SES" },
+                    { title: "SFN (Step Functions)" },
+                    { title: "SNS" },
+                    { title: "SWF Domain" },
+                    { title: "Synthetics" },
+                    { title: "Transcribe" },
+                    { title: "Transfer" },
+                    { title: "Verified Access" },
+                    { title: "WorkLink" },
+                    { title: "WorkSpaces" },
+                    { title: "XRay" }
+                ]
+
+            },
+            {
+                title: 'AI',
+                subTabs: [{ title: "Kendra" },
+                { title: "Lex" },
+                { title: "Sagemaker" }]
+            },
+            {
+                title: "Analytics",
+                subTabs: [{ title: "Athena" },
+                { title: "CloudSearch" },
+                { title: "Data Exchange" },
+                { title: "ElasticSearch" },
+                { title: "EMR" },
+                { title: "Glue" },
+                { title: "Kinesis" },
+                { title: "Lake Formation" },
+                { title: "QuickSight" },
+                { title: "Redshift" }]
+            },
+            {
+                title: "Applications",
+                subTabs: [{ title: "Amplify Console" },
+                { title: "AppConfig" },
+                { title: "AppFlow" },
+                { title: "AppIntegrations" },
+                { title: "AppStream" },
+                { title: "AppSync" },
+                { title: "Chime" },
+                { title: "Connect" },
+                { title: "Device Farm" },
+                { title: "Elastic Transcoder" },
+                { title: "Elemental MediaLive" },
+                { title: "Gamelift" },
+                { title: "IVS (Interactive Video)" },
+                { title: "Media Convert Queue" },
+                { title: "Media Package Channel" },
+                { title: "MediaStore" }]
+            },
+            {
+                title: "IOT",
+                subTabs: [{ title: "IOT" },
+                ]
+            },
+            {
+                title: "Other",
+                subTabs: [{ title: "ACM" },
+                { title: "CloudHSM v2" },
+                { title: "Comprehend Document Classifier" },
+                { title: "DataPipeline" },
+                { title: "DLM (Data Lifecycle Manager)" },
+                { title: "FIS (Fault Injection Simulator)" },
+                { title: "Kafka Connect (MSK Connect)" },
+                { title: "Location" },
+                { title: "Macie" },
+                { title: "MSK" },
+                { title: "MWAA (Managed Workflows for Apache Airflow)" },
+                { title: "OpenSearch" },
+                { title: "RAM" },
+                { title: "Recycle Bin" },
+                { title: "Roles Anywhere" },
+                { title: "Service Quotas" },
+                { title: "SES" },
+                { title: "SNS" },
+                { title: "SQS" },
+                { title: "SSM" },
+                { title: "Step Function (SFN)" }
+                ]
+            },
         ],
-      },
-      {
-        title: 'Network',
-        subTabs: [
-          {
-            title: 'API Gateway',
-          },
-          {
-            title: 'AppMesh',
-          },
-          {
-            title: 'CloudFront',
-          },
-          {
-            title: 'Direct Connect',
-          },
-          {
-            title: 'Global Accelerator',
-          },
-          {
-            title: 'Load Balancers',
-          },
-          {
-            title: 'Network Firewall',
-          },
-          {
-            title: 'Network Manager',
-          },
-          {
-            title: 'Route 53',
-          },
-          {
-            title: 'Transit Gateway',
-          },
-          {
-            title: 'VPC',
-          },
-          {
-            title: 'VPN',
-          },
-        ],
-      },
-      {
-        title: 'Storage',
-        subTabs: [
-          {
-            title: 'Archieve & Backup',
-          },
-          {
-            title: 'Backup',
-          },
-          {
-            title: 'File System',
-          },
-          {
-            title: 'S3',
-          },
-          {
-            title: 'Storage Gateway',
-          },
-        ],
-      },
-      {
-        title: 'Database',
-        subTabs: [
-          {
-            title: 'DMS',
-          },
-          {
-            title: 'DocumentDB',
-          },
-          {
-            title: 'Dynamo',
-          },
-          {
-            title: 'S3',
-          },
-          {
-            title: 'Storage Gateway',
-          },
-        ],
-      },
-    ],
-  },
+    },
 };
